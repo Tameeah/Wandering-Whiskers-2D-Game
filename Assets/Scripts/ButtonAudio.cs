bafcc3687543e15cb664a12d82351097db90bd46
Tameeah <tameeah.padayachy@gmail.com>
2025-05-13 18:33:34 +0200

    Button Audio

--- Assets/Scripts/ButtonAudio.cs
+++ Assets/Scripts/ButtonAudio.cs
@@ -1,16 +1,27 @@
 using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
 
 public class ButtonAudio : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public AudioClip sound;
+
+    private Button button { get { return GetComponent<Button>(); } }
+    private AudioSource source {  get {  return GetComponent<AudioSource>(); } }
+
     void Start()
     {
-        
+        gameObject.AddComponent<AudioSource>();
+        source.clip = sound;
+        source.playOnAwake = false;
+
+        button.onClick.AddListener(()=> PlaySound());
     }
 
-    // Update is called once per frame
-    void Update()
+    void PlaySound()
     {
-        
+        source.PlayOneShot(sound);
     }
 }
