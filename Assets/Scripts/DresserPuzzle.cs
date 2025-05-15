45124fc5b035504ef93d57f4df2709a8bc9ae0d3
Tameeah <tameeah.padayachy@gmail.com>
2025-05-15 20:22:29 +0200

    Level 2 Updated

---
+++ Assets/Scripts/DresserPuzzle.cs
@@ -0,0 +1,111 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class DresserPuzzle : MonoBehaviour
+{
+    public GameObject pink, orange, peach, red, yellow, pinkHolder, orangeHolder, peachHolder, redHolder, yellowHolder;
+    Vector2 pinkInitialPos, orangeInitialPos, peachInitialPos, redInitialPos, yellowInitialPos;
+    private bool isPlaced = false;
+    private bool isPlacedCorrectly;
+
+    void Start()
+    {
+        pinkInitialPos = pink.transform.position;
+        orangeInitialPos = orange.transform.position;
+        peachInitialPos = peach.transform.position;
+        redInitialPos = red.transform.position;
+        yellowInitialPos = yellow.transform.position;
+    }
+    public void DragPink()
+    {
+        pink.transform.position = Input.mousePosition;
+    }
+
+    public void DragOrange()
+    {
+        orange.transform.position = Input.mousePosition;
+    }
+
+    public void DragPeach()
+    {
+        peach.transform.position = Input.mousePosition;
+    }
+
+    public void DragRed()
+    {
+        red.transform.position = Input.mousePosition;
+    }
+
+    public void DragYellow()
+    {
+        yellow.transform.position = Input.mousePosition;
+    }
+
+    public void DropPink()
+    {
+        float Distance = Vector3.Distance(pink.transform.position, pinkHolder.transform.position);
+        if (Distance < 50)
+        {
+            pink.transform.position = pinkHolder.transform.position;
+        }
+        else
+        {
+            pink.transform.position = pinkInitialPos;
+        }
+    }
+    public void DropOrange()
+    {
+        float Distance = Vector3.Distance(orange.transform.position, orangeHolder.transform.position);
+        if (Distance < 50)
+        {
+            orange.transform.position = orangeHolder.transform.position;
+        }
+        else
+        {
+            orange.transform.position = orangeInitialPos;
+        }
+    }
+    public void DropPeach()
+    {
+        float Distance = Vector3.Distance(peach.transform.position, peachHolder.transform.position);
+        if (Distance < 50)
+        {
+            peach.transform.position = peachHolder.transform.position;
+        }
+        else
+        {
+            peach.transform.position = peachInitialPos;
+        }
+    }
+    public void DropRed()
+    {
+        float Distance = Vector3.Distance(red.transform.position, redHolder.transform.position);
+        if (Distance < 50)
+        {
+            red.transform.position = redHolder.transform.position;
+        }
+        else
+        {
+            red.transform.position = redInitialPos;
+        }
+    }
+    public void DropYellow()
+    {
+        float Distance = Vector3.Distance(yellow.transform.position, yellowHolder.transform.position);
+        if (Distance < 50)
+        {
+            yellow.transform.position = yellowHolder.transform.position;
+        }
+        else
+        {
+            yellow.transform.position = yellowInitialPos;
+        }
+    }
+    public void Place()
+    {
+        isPlacedCorrectly = true;
+        gameObject.SetActive(false);
+        PuzzleManager.instance.PiecesPlacedCorrectly();
+    }
+}
